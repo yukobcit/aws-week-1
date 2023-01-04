6d9f054f85fac84b0f3926ee5c8eb08290db23da
Yuko Ishida <yishida2@my.bcit.ca>
2023-01-04 14:08:02 -0800

    pikachu added

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.get("/", (req, res) => {
-  console.log("GET /")
-  res.send("<h1>hello aws</h1>")
+app.use(express.json())
+
+const pokemons = [
+  {
+    id: 1,
+    name: "Pikachu",
+    type: "electric ⚡️",
+    level: 99,
+    image: "/pikachu.webp"
+  }
+]
+
+app.get("/api/pokemons", (req, res) => {
+  console.log("GET /api/pokemons")
+  res.send({pokemons: pokemons})
+});
+
+app.post("/api/pokemons", (req, res) => {
+  const data = req.body
+  console.log("POST /api/pokemons", data)
+  data.id = pokemons.length+1
+  pokemons.push(data)
+  res.send(data)
 })
 
 const port = process.env.PORT || 8080
